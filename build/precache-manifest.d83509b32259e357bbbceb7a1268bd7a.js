a4b7ce79943fbd492a45708727a5da4371b88172
Jaimie-Leigh Firth <jaimie-leigh-firth@Hotmail.co.uk>
2020-12-14 13:52:30 +0000

    build files

---
+++ build/precache-manifest.d83509b32259e357bbbceb7a1268bd7a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fd7242b0466fcdcf52f64c8b39a4e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13b4904d9b52bacfb388",
+    "url": "/static/css/main.3fffb231.chunk.css"
+  },
+  {
+    "revision": "3ffb407ab7fb3e7bf227",
+    "url": "/static/js/2.0cd98719.chunk.js"
+  },
+  {
+    "revision": "ce53c4b16522e4a5817476f862996bac",
+    "url": "/static/js/2.0cd98719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b4904d9b52bacfb388",
+    "url": "/static/js/main.83cb3b36.chunk.js"
+  },
+  {
+    "revision": "1c18569f69b9cfe9533d",
+    "url": "/static/js/runtime-main.92a1bfc4.js"
+  },
+  {
+    "revision": "a172507a32ae16a41389aef128d50051",
+    "url": "/static/media/chat.a172507a.svg"
+  },
+  {
+    "revision": "4a2245249c6ff9a583036334bd8b1036",
+    "url": "/static/media/down-chevron.4a224524.svg"
+  },
+  {
+    "revision": "b808289c6060c4a32cd3085af61e2ec5",
+    "url": "/static/media/halifax_logo.b808289c.svg"
+  },
+  {
+    "revision": "f29dfa384c0efd7b5263bb65782eb902",
+    "url": "/static/media/home-black.f29dfa38.svg"
+  },
+  {
+    "revision": "c6f1a90fc940f14a8d286b3c598b7024",
+    "url": "/static/media/question.c6f1a90f.svg"
+  },
+  {
+    "revision": "16226f88ff53f9cc624114c6f218eb68",
+    "url": "/static/media/up-chevron.16226f88.svg"
+  }
+]);
